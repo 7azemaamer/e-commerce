7a53d3b5694c5b3566329c716403e9e0aa8f0448
7azemaamer <125705935+7azemaamer@users.noreply.github.com>
2024-05-11 02:10:14 +0300

    changed images

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,7 +6,6 @@ import { Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 import { UserToken } from '../../Context/UserToken';
 import { Helmet } from 'react-helmet';
-import toast from 'react-hot-toast';
 export default function Login() {
 
   let navigate = useNavigate();
--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./mediaQuery.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.js";
-import { QueryClient, QueryClientProvider } from "react-query";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import "./mediaQuery.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
+import { QueryClient, QueryClientProvider } from "react-query"
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 //Provide all project with query
-let queryClient = new QueryClient();
+let queryClient = new QueryClient()
 
 root.render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-         <App />
-      </QueryClientProvider>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
-);
-reportWebVitals();
+)
+reportWebVitals()
