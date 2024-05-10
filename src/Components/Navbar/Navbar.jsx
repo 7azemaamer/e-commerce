f03de5dcce2904ef5ea7eb1cf49d14566a0fc0ec
7azemaamer <125705935+7azemaamer@users.noreply.github.com>
2024-05-11 02:24:25 +0300

    fixing responsive for mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,78 +1,98 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Link, NavLink, useNavigate } from "react-router-dom";
-import logo from "../../Assets/images/freshcart-logo.svg";
-import { UserToken } from "../../Context/UserToken";
-import { CartContext } from "../../Context/CartContext";
-import { WishListContext } from "../../Context/WishListContext";
+import React, { useContext, useEffect, useState } from "react"
+import { Link, NavLink, useNavigate } from "react-router-dom"
+import logo from "../../Assets/images/freshcart-logo.svg"
+import { UserToken } from "../../Context/UserToken"
+import { CartContext } from "../../Context/CartContext"
+import { WishListContext } from "../../Context/WishListContext"
 
 export default function Navbar() {
   let navigate = useNavigate()
-  let {userToken , setUserToken} =useContext(UserToken);
-  let { numOfCartItems , setNumOfCartItems,  getLoggedUserCart , isCartEmpty , setIsCartEmpty} = useContext(CartContext);
-  let {countWishlist , getLoggedWishlist , setCountWishlist} = useContext(WishListContext);
+  let { userToken, setUserToken } = useContext(UserToken)
+  let {
+    numOfCartItems,
+    setNumOfCartItems,
+    getLoggedUserCart,
+    isCartEmpty,
+    setIsCartEmpty,
+  } = useContext(CartContext)
+  let { countWishlist, getLoggedWishlist, setCountWishlist } =
+    useContext(WishListContext)
 
   useEffect(() => {
     getLoggedWishlist()
       .then((response) => {
         if (response?.data.status === "success") {
-          setCountWishlist(response?.data?.count);
-          localStorage.setItem("wishListCount", response?.data?.count);
+          setCountWishlist(response?.data?.count)
+          localStorage.setItem("wishListCount", response?.data?.count)
         }
       })
-      .catch((error) => console.error("Error fetching wishlist count:", error));
-  
+      .catch((error) => console.error("Error fetching wishlist count:", error))
+
     if (!isCartEmpty) {
       getLoggedUserCart()
         .then((response) => {
           if (response.data?.status === "success") {
-            setNumOfCartItems(response.data?.count);
-            localStorage.setItem("wishListCount", response.data?.count);
+            setNumOfCartItems(response.data?.count)
+            localStorage.setItem("wishListCount", response.data?.count)
           }
         })
-        .catch((error) => console.error("Error fetching cart count:", error));
+        .catch((error) => console.error("Error fetching cart count:", error))
     }
-  }, [isCartEmpty ]);
-  
-  function logOut(){
-    localStorage.removeItem("loginToken");
-    setUserToken(null);
-    localStorage.removeItem("isCartEmpty");
-    navigate("login");
+  }, [isCartEmpty])
+
+  function logOut() {
+    localStorage.removeItem("loginToken")
+    setUserToken(null)
+    localStorage.removeItem("isCartEmpty")
+    navigate("login")
   }
-  return <>
+  return (
+    <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
         <div className="container">
           <Link className="navbar-brand" to={"/"}>
             <img src={logo} alt="FreshCart" />
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            {userToken?<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink className="nav-link" to={"/"}>
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to={"products"}>
-                  Products
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to={"/categories"}>
-                  Categories
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to={"/brands"}>
-                  Brands
-                </NavLink>
-              </li>
-            </ul>: ""}
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+            {userToken ? (
+              <ul className="navbar-nav me-auto mb-md-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to={"/"}>
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to={"products"}>
+                    Products
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to={"/categories"}>
+                    Categories
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to={"/brands"}>
+                    Brands
+                  </NavLink>
+                </li>
+              </ul>
+            ) : (
+              ""
+            )}
+            <ul className="navbar-nav ms-auto mb-md-2 mb-lg-0">
+              <li className="nav-item socialMedia">
                 <div className="nav-link arrangeLinks">
                   <a target="_blank" href="https://www.facebook.com/7azemaamer">
                     <i className="fab fa-instagram me-1"></i>
@@ -94,40 +114,60 @@ export default function Navbar() {
                   </a>
                 </div>
               </li>
-              {userToken?<>
-                <li className="nav-item">
-                <span onClick={()=>logOut()} className="nav-link cursor-pointer">
-                  Logout
-                </span>
-              </li>
-              <li className="nav-item d-flex align-items-center">
-                <Link to={'profile'}>
-                  <i className="fa-solid fa-user fs-4 me-3"></i>
-                </Link>
-                <Link className="position-relative" to={"/cart"}>
-                  <i className="fa-solid fs-4 pt-1 fa-cart-shopping text-main me-3"></i>
-                  {numOfCartItems!==0 ? <div className="bg-danger font-sm-2 text-white position-absolute top-50 start-50 translate-middle-x rounded-circle ">{numOfCartItems}</div> :""}
-                </Link>
-                <Link to={'wishlist'}>
-                  <i className="fa-solid fs-3 fa-heart mt-1 text-main"></i>
-                  {Number(countWishlist)!==0 ? <div className="bg-danger font-sm-2 text-white position-absolute top-50 start-50 translate-middle-x rounded-circle px-1">{countWishlist}</div> :""}
-                </Link>
-              </li>
-              </>:<>
-              <li className="nav-item d-flex align-items-center me-3">
-                <Link className="nav-Link" to={"login"}>
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item d-flex align-items-center mx-3">
-                <Link className="nav-Link" to={"register"}>
-                  Register
-                </Link>
-              </li>
-              </>}
+              {userToken ? (
+                <>
+                  <li className="nav-item">
+                    <span
+                      onClick={() => logOut()}
+                      className="nav-link cursor-pointer"
+                    >
+                      Logout
+                    </span>
+                  </li>
+                  <li className="nav-item d-flex align-items-center pfp">
+                    <Link to={"profile"}>
+                      <i className="fa-solid fa-user fs-4 me-3"></i>
+                    </Link>
+                    <Link className="position-relative" to={"/cart"}>
+                      <i className="fa-solid fs-4 pt-1 fa-cart-shopping text-main me-3"></i>
+                      {numOfCartItems !== 0 ? (
+                        <div className="bg-danger font-sm-2 text-white position-absolute top-50 start-50 translate-middle-x rounded-circle ">
+                          {numOfCartItems}
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </Link>
+                    <Link to={"wishlist"}>
+                      <i className="fa-solid fs-3 fa-heart mt-1 text-main"></i>
+                      {Number(countWishlist) !== 0 ? (
+                        <div className="bg-danger font-sm-2 text-white position-absolute top-50 start-50 translate-middle-x rounded-circle px-1">
+                          {countWishlist}
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item d-flex align-items-center me-3">
+                    <Link className="nav-Link" to={"login"}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item d-flex align-items-center mx-3">
+                    <Link className="nav-Link" to={"register"}>
+                      Register
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
       </nav>
     </>
+  )
 }
--- src/Components/PopulerHomeSlider/PopulerHomeSlider.jsx
+++ src/Components/PopulerHomeSlider/PopulerHomeSlider.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import Style from  "./PopulerHomeSlider.module.css"
-import axios from 'axios';
-import { useQuery } from 'react-query';
-import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
+import React from "react"
+import Style from "./PopulerHomeSlider.module.css"
+import axios from "axios"
+import { useQuery } from "react-query"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 export default function PopulerHomeSlider() {
-
-  function getPopularProducts(){
-    return axios.get(`https://ecommerce.routemisr.com/api/v1/categories`);
+  function getPopularProducts() {
+    return axios.get(`https://ecommerce.routemisr.com/api/v1/categories`)
   }
-  let {data , isError , isFetching} = useQuery("popularProducts", getPopularProducts);
+  let { data, isError, isFetching } = useQuery(
+    "popularProducts",
+    getPopularProducts
+  )
 
   const settings = {
     dots: true,
@@ -20,7 +21,7 @@ export default function PopulerHomeSlider() {
     speed: 500,
     slidesToShow: 7,
     slidesToScroll: 2,
-    autoplay:true,
+    autoplay: true,
     autoplaySpeed: 2000,
     cssEase: "linear",
     responsive: [
@@ -30,8 +31,8 @@ export default function PopulerHomeSlider() {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots: false
-        }
+          dots: false,
+        },
       },
       {
         breakpoint: 992,
@@ -39,19 +40,27 @@ export default function PopulerHomeSlider() {
           slidesToShow: 4,
           slidesToScroll: 1,
           infinite: true,
-          dots: false
-        }
-      }
-    ]
-  };
-  return <div className='my-3'>
-      <h3 className='mb-3'>Shop Popular Category</h3>
+          dots: false,
+        },
+      },
+    ],
+  }
+  return (
+    <div className="my-3">
+      <h3 className="mb-3">Shop Popular Category</h3>
       <Slider {...settings}>
-        {data?.data.data.map((category )=><div key={category._id}>
-          <img className='w-100' height={250} src={category.image} alt={category.name}/>
-          <div className='mt-2 fw-bold text-center'>{category.name}</div>
-        </div>
-        )}
+        {data?.data.data.map((category) => (
+          <div className="shopPopulerCat" key={category._id}>
+            <img
+              className="w-100"
+              height={250}
+              src={category.image}
+              alt={category.name}
+            />
+            <div className="mt-2 fw-bold text-center">{category.name}</div>
+          </div>
+        ))}
       </Slider>
-  </div>
+    </div>
+  )
 }
